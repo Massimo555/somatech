aaeb2ecffbf3840be8f2f1c6561a0e46368a0f6e
massimo555 <massimomaxima@gmail.com>
2019-12-18 12:24:13 -0800

    production

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const mongoURI = require('./config').mongoURI
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const mongoURI = require("./config").mongoURI;
 
 //middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //database connection
-mongoose.connect(mongoURI, {useNewUrlParser: true,
-useCreateIndex: true, useUnifiedTopology: true  }, ()=>{
- console.log('Connected to the database');
-})
+mongoose.connect(
+  mongoURI,
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to the database");
+  }
+);
 
-mongoose.set('useFindAndModify', false) //used because of deprecation issue
+mongoose.set("useFindAndModify", false); //used because of deprecation issue
 
-//routes
-app.use('/api', require('./routes/post'))
+//routes routes
+app.use("/api", require("./routes/post"));
 
-const PORT = 5000
-app.listen(PORT, ()=>console.log(`The server is running on port ${PORT}`))
+//i used PORT 5000
+const PORT = 5000;
+app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
